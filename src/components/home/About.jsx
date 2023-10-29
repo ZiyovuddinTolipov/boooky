fda98396efdc9aee4ac5cae34e8d57965c675042
tolipov <ziyovuddin.developer@gmail.com>
2023-10-29 14:51:25 +0500

    about

---
+++ src/components/home/About.jsx
@@ -0,0 +1,25 @@
+
+const About = () => {
+    return (
+        <main className="bg-[#F2F2F2] py-10 mt-10">
+            <section className="max-w-[1400px] mx-auto">
+                <h2>Lorem ipsum dolor sit.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <div className="">
+                    <div>
+                        <div></div>
+                        <h3>At School</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea itaque id amet odit veritatis,
+                            commodi nostrum eaque iusto dignissimos soluta!
+                        </p>
+                        <button>
+                            learn more
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default About
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/home/Navbar"
 import Hero from "../components/home/Hero"
+import About from "../components/home/About"
 const Home = () => {
     return (
         <main className="max-w-[1600px] mx-auto">
             <Navbar />
             <Hero />
+            <About />
         </main>
     )
 }
