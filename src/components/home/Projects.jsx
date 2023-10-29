46812a613fe8535b91b761f4732f972bd3f08477
tolipov <ziyovuddin.developer@gmail.com>
2023-10-29 16:59:14 +0500

    projects page

---
+++ src/components/home/Projects.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+
+const Projects = () => {
+    const style = {
+        sliderDiv: "h-[200px] bg-black mx-3 text-white text-center",
+
+    }
+    return (
+        <div className='my-6'>
+            <h3 className='text-3xl font-semibold my-4 text-center'>Amazing things are happening</h3>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation,Autoplay]}
+                className="mySwiper"
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+            >
+                <SwiperSlide className={style.sliderDiv}>
+                    {({ isActive }) => (
+                        <div>Current slide is {isActive ? 'active' : 'not active'}</div>
+                    )}
+                </SwiperSlide>
+                <SwiperSlide className={style.sliderDiv}>Slide 2</SwiperSlide>
+                <SwiperSlide className={style.sliderDiv}>Slide 3</SwiperSlide>
+                <SwiperSlide className={style.sliderDiv}>Slide 4</SwiperSlide>
+                <SwiperSlide className={style.sliderDiv}>Slide 5</SwiperSlide>
+                <SwiperSlide className={style.sliderDiv}>Slide 6</SwiperSlide>
+                <SwiperSlide className={style.sliderDiv}>Slide 7</SwiperSlide>
+            </Swiper>
+        </div>
+    )
+}
+
+export default Projects
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Navbar from "../components/home/Navbar"
 import Hero from "../components/home/Hero"
 import About from "../components/home/About"
+import Projects from "../components/home/Projects"
+
 const Home = () => {
     return (
         <main className="max-w-[1600px] mx-auto">
             <Navbar />
             <Hero />
             <About />
+            <Projects />
         </main>
     )
 }
