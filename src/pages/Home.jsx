4df315c074f23580d10452f68c4abd6b5d18b5f9
tolipov <ziyovuddin.developer@gmail.com>
2023-10-29 12:50:09 +0500

    hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import Navbar from "../components/home/Navbar"
 import Hero from "../components/home/Hero"
 const Home = () => {
     return (
-        <>
+        <main className="max-w-[1600px] mx-auto">
             <Navbar />
             <Hero />
-        </>
+        </main>
     )
 }
 
